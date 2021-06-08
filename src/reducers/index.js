25c63aea264f71c5ed94877e74f0b6a4e79d15e4
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-08 19:16:19 -0300

    Presetting redux project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,13 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+/* {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+} */
