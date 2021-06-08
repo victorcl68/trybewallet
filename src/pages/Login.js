9ed6d32046d223196485487dfba2ab94b0d62cc4
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-08 19:19:26 -0300

    First Requirement resolution, login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <label htmlFor="email-input-id">
+          E-mail
+          <input type="email" data-testid="email-input" id="email-input-id" />
+        </label>
+        <label htmlFor="password-input-id">
+          Senha
+          <input type="password" data-testid="password-input" id="password-input-id" />
+        </label>
+        <button type="button">
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
