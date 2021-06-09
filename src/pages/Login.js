165007ab8ca87445a63ce6b1c494882155c9277c
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-09 00:55:36 -0300

    Little adjustments

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,19 +3,36 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <>
-        <label htmlFor="email-input-id">
-          E-mail
-          <input type="email" data-testid="email-input" id="email-input-id" />
-        </label>
-        <label htmlFor="password-input-id">
-          Senha
-          <input type="password" data-testid="password-input" id="password-input-id" />
-        </label>
-        <button type="button">
-          Entrar
-        </button>
-      </>
+      <main>
+        <form>
+          <label htmlFor="email-input-id">
+            E-mail
+            <input
+              type="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+              // RegEx retirado desse site: https://wbruno.com.br/html/validando-formularios-apenas-com-html5/
+              placeholder="Coloque seu email aqui!"
+              data-testid="email-input"
+              id="email-input-id"
+              required
+            />
+          </label>
+          <label htmlFor="password-input-id">
+            Senha
+            <input
+              type="password"
+              placeholder="Insira sua senha aqui!"
+              minLength="6"
+              data-testid="password-input"
+              id="password-input-id"
+              required
+            />
+          </label>
+          <button type="submit">
+            Entrar
+          </button>
+        </form>
+      </main>
     );
   }
 }
