f54cf24facbb9cdea5987acc54155080b62a3ff2
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-13 18:39:04 -0300

    changing user reducer name to "user" only

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,11 +2,11 @@ const INITIAL_STATE = {
   email: '',
 };
 
-const userReducer = (state = INITIAL_STATE, action) => {
+const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   default:
     return state;
   }
 };
 
-export default userReducer;
+export default user;
