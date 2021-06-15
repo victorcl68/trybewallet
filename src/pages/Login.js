95a924568b3c3f71ea53adee24cb60f1c3ed553d
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-14 22:59:09 -0300

    Importing connect, proptypes and action

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
+import emailData from '../actions';
 
 class Login extends React.Component {
   render() {
