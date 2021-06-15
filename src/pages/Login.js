4c9044bffdf9c0f07a95a492851b0ddd857a89e9
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-14 23:01:37 -0300

    Refactoring my entire main element with the inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -52,33 +52,34 @@ class Login extends React.Component {
     return (
       <main>
         <form>
-          <h1>Trybe Wallet</h1>
-          <label htmlFor="email-input-id">
-            E-mail
-            <input
-              type="email"
-              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-              // RegEx retirado desse site: https://wbruno.com.br/html/validando-formularios-apenas-com-html5/
-              placeholder="Coloque seu email aqui!"
-              data-testid="email-input"
-              id="email-input-id"
-              required
-            />
-          </label>
-          <label htmlFor="password-input-id">
-            Senha
-            <input
-              type="password"
-              placeholder="Insira sua senha aqui!"
+        <h1>Trybe Wallet</h1>
+        <label htmlFor="email-input-id">
+          E-mail
+          <input
+            type="email"
+            name="email"
+            placeholder="Coloque seu email aqui!"
+            data-testid="email-input"
+            id="email-input-id"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input-id">
+          Senha
+          <input
+            type="password"
+            minLength="6"
+            placeholder="Insira sua senha aqui!"
               minLength="6"
-              data-testid="password-input"
-              id="password-input-id"
-              required
-            />
-          </label>
-          <button type="submit">
-            Entrar
-          </button>
+            data-testid="password-input"
+            id="password-input-id"
+            name="password"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="button" onClick={ this.handleClick } disabled={ isButtonDisabled }>
+          Entrar
+        </button>
         </form>
       </main>
     );
