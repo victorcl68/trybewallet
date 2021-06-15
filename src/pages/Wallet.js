e5bb31ce754d19f26aa6d9dd82940e021d15c6c2
Victor Lopes <victor.cabrera0209@gmail.com>
2021-06-14 23:49:34 -0300

    Adding form to the Wallet Page

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,14 +6,50 @@ class Wallet extends React.Component {
   render() {
     const { emailUser } = this.props;
     return (
-      <main>
-        <h3 data-testid="email-field">
-          Email:
-          { emailUser }
-        </h3>
-        <h3 data-testid="total-field"> 0 </h3>
-        <h3 data-testid="header-currency-field"> BRL </h3>
-      </main>
+      <>
+        <header>
+          <h3 data-testid="email-field">
+            Email:
+            { emailUser }
+          </h3>
+          <h3 data-testid="total-field"> 0 </h3>
+          <h3 data-testid="header-currency-field"> BRL </h3>
+        </header>
+        <form>
+          <label htmlFor="valor">
+            Valor:
+            <input type="number" id="valor" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição:
+            <input type="text" id="descricao" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda:
+            <select>
+              <option> </option>
+            </select>
+          </label>
+          <label htmlFor="metodo">
+            Método de pagamento:
+            <select>
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag:
+            <select>
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
