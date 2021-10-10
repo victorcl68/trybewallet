0d4f54e90a0204dbaf8082ff36406a100dc075f4
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-10 16:40:36 -0300

    Updates

---
+++ precache-manifest.1ca978f5d31256e00751a44d314d3fc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e79abb65fef73b0bc45e760d3cb68f",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "b6de3e793a514c12a08c",
+    "url": "/trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c295785a3410ac1b70a6",
+    "url": "/trybewallet/static/js/2.0309981b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.0309981b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6de3e793a514c12a08c",
+    "url": "/trybewallet/static/js/main.32ddf468.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
